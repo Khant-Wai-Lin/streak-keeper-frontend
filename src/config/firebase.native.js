3944c3bd544fa21c2e53d743ee520721c570350d
Khant-Wai-Lin <6731503060@lamduan.mfu.ac.th>
2026-04-15 01:36:53 +0700

    fixing environment variable

--- src/config/firebase.native.js
+++ src/config/firebase.native.js
@@ -2,28 +2,33 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const requiredKeys = [
-  "EXPO_PUBLIC_FIREBASE_API_KEY",
-  "EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN",
-  "EXPO_PUBLIC_FIREBASE_PROJECT_ID",
-  "EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET",
-  "EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID",
-  "EXPO_PUBLIC_FIREBASE_APP_ID",
-];
+const FIREBASE_API_KEY = process.env.EXPO_PUBLIC_FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const FIREBASE_PROJECT_ID = process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = process.env.EXPO_PUBLIC_FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID;
 
-const missingKeys = requiredKeys.filter((key) => !process.env[key]);
+const missingKeys = [];
+if (!FIREBASE_API_KEY) missingKeys.push("EXPO_PUBLIC_FIREBASE_API_KEY");
+if (!FIREBASE_AUTH_DOMAIN) missingKeys.push("EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN");
+if (!FIREBASE_PROJECT_ID) missingKeys.push("EXPO_PUBLIC_FIREBASE_PROJECT_ID");
+if (!FIREBASE_STORAGE_BUCKET) missingKeys.push("EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET");
+if (!FIREBASE_MESSAGING_SENDER_ID) missingKeys.push("EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID");
+if (!FIREBASE_APP_ID) missingKeys.push("EXPO_PUBLIC_FIREBASE_APP_ID");
 if (missingKeys.length > 0) {
   throw new Error(`Missing Firebase env vars: ${missingKeys.join(", ")}`);
 }
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
